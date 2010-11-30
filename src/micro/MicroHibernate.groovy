6de70f7b2258580c094d893892980f15fec2b504
Andre Steingress <andre@isangria.lan>
2010-11-30 20:01:13 +0100

    added snapshots, discarding functionality

--- src/micro/MicroHibernate.groovy
+++ src/micro/MicroHibernate.groovy
@@ -1,104 +1,138 @@
-package micro
+class SessionFactory {
 
-class SessionFactory  {
+    private def storage
 
-  private def storage
+    def SessionFactory(def storage)  {
+        this.storage = storage
+    }
 
-  def SessionFactory(def storage)  {
-    this.storage = storage
-  }
-
-  def createNewSession()  {
-    new Session(this)
-  }
-
-  def newStorageConnection()  {
-    return this.storage
-  }
+    def newStorageConnection()  {
+        return storage
+    }
+    
+    def newSession()  {
+        return new Session(this)
+    }
 }
 
 class Session {
 
-  private def sessionFactory
+    private def sessionFactory
 
-  private def keyMap
-  private def keyModificationListMap
+    def Session(def sessionFactory) { this.sessionFactory = sessionFactory }
 
-  def Session(def sessionFactory)  {
-    this.sessionFactory = sessionFactory
-    this.keyMap = [:]
-    this.keyModificationListMap = [:]
-  }
-
-  def propertyChanged(def obj)  {
-    if (!keyModificationListMap[obj.class]) keyModificationListMap[obj.getClass()] = []
-    keyModificationListMap[obj.class] = obj.id
-  }
-
-  def save()  {
-    println "going to save all modified objects"
-
-    keyModificationListMap.each {
-      def domainClass ->
-        println "Modified objects: ${domainClass}"
+    def snapshots = [:] // a Map(Domain-Class, Map(Identifier, Properties))
+    def identityMap = [:] // a Map(Domain-Class, Map(Identifier, ObjectRef))
+    def modifiedPersistentObjects = [:] // a Map(Domain-Class, List(Identifier))
+    
+    def propertyChanged(def obj)  {
+        if (!modifiedPersistentObjects[obj.getClass()]) modifiedPersistentObjects[obj.getClass()] = []
+        
+        modifiedPersistentObjects[obj.getClass()] << obj.id
     }
-  }
-
-  def delete(def obj)  {
-     // TODO: implement different state changing events to signal deletes, saves, updated etc.
-  }
-
-  def load(def domainClass, long identifier)  {
-    if (keyMap[domainClass] && keyMap[domainClass][identifier]) return keyMap[domainClass][identifier]
-
-    // load object from underlying storage
-    def connection = sessionFactory.newStorageConnection()
-    def loadedObj = connection[domainClass][identifier]
-    if (!loadedObj) throw new Exception("Object with ${identifier} could not be found")
-
-    // register object in entity map
-    if (!keyMap[domainClass]) keyMap[domainClass] = [:]
-    if (!keyMap[domainClass][identifier]) keyMap[domainClass][identifier] = loadedObj
-
-    injectIdProperty(loadedObj, identifier)
-    injectPropertyChangedCallback(loadedObj)
-
-    return loadedObj
-  }
-
-  private def injectIdProperty(loadedObj, long identifier) {
-    loadedObj.metaClass.getId = {-> identifier }
-  }
-
-  private def injectPropertyChangedCallback(loadedObj)  {
-    loadedObj.metaClass.setProperty = { String name, Object value ->
-      println "${name} invoked"
-
-      owner.propertyChanged(loadedObj)
-
-      def metaProperty = delegate.metaClass.getMetaProperty(name)
-      if(metaProperty) metaProperty.setProperty(delegate, value)
+    
+    def save(def obj) {
+        if (modifiedPersistentObjects[obj.getClass()] && modifiedPersistentObjects[obj.getClass()][obj.id])  {
+           // something changed
+           
+           // what changed? did a simple property change, did the contents of a relationship change? etc.
+           
+        } else if (!obj.metaClass.hasProperty("id"))  {
+           // this is a transient object
+           
+           // get the id for this object
+        }
     }
-  }
+    
+    def discard()  {
+        // reverts all changes
+        modifiedPersistentObjects.each {
+            def domainClassType, def listOfIds -> 
+                def l = listOfIds.clone()
+                
+                l.each {
+                    def alteredObj = identityMap[domainClassType][it]
+                    def oldPropertyValues = snapshots[domainClassType][it]
+                    
+                    oldPropertyValues.each { name, value -> if (name in ['name']) alteredObj[name] = value }
+                }
+        }
+        
+        clear()
+    }
+    
+    def flush()  {
+        if (modifiedPersistentObjects) println "flushing ..."
+        
+        clear()
+    }
+    
+    def clear()  {
+        snapshots.clear()
+        identityMap.clear()
+        modifiedPersistentObjects.clear()
+    }
+    
+    def close()  {
+        flush()
+        clear()
+        
+        println "session is cleared."
+    }
+    
+    def load(def domainClassType, Long identifier)  {
+        
+        if (identityMap[domainClassType] && identityMap[domainClassType][identifier])  {
+            return identityMap[domainClassType][identifier]
+        }
+        
+        def conn = sessionFactory.newStorageConnection()
+        def loadedObj = conn[domainClassType][identifier]
+        if (!loadedObj) throw new Exception("Object of type ${domainClassType} with id ${identifier} could not be found!")
+        
+        if (!snapshots[domainClassType]) snapshots[domainClassType] = [:]
+        if (!identityMap[domainClassType]) identityMap[domainClassType] = [:]
+        
+        snapshots[domainClassType][identifier] = loadedObj.properties
+        identityMap[domainClassType][identifier] = loadedObj
+        
+        loadedObj.metaClass.getId = { -> identifier } 
+        loadedObj.metaClass.setProperty = { String name, Object arg ->
+            def metaProperty = delegate.metaClass.getMetaProperty(name)
+
+            if (metaProperty)  {
+                owner.propertyChanged(loadedObj)
+                
+                metaProperty.setProperty(delegate, arg)
+            }
+        }
+        
+        return loadedObj
+    }
+    
+    def query(def q)  {
+        def ids = executeQuery(q) // returns a list of ids
+        // lookup ids in cache if not found load them from storage/DB
+    }
 }
 
-class Person { String name }
-
 def storage = [:]
-storage[Person.class] = [:]
-storage[Person.class][1l] = new Person(name: 'Max Mustermann')
-storage[Person.class][2l] = new Person(name: 'Erika Mustermann')
+
+class Person {
+    String name
+}
+
+storage[Person] = [:]
+storage[Person][1 as Long] = new Person(name: 'Max Mustermann')
+storage[Person][2 as Long] = new Person(name: 'Erika Mustermann')
 
 def sessionFactory = new SessionFactory(storage)
-def session = sessionFactory.createNewSession()
+def session = sessionFactory.newSession()
 
 def person = session.load(Person, 1)
+person.name = 'Franz Xaver Gabler'
 
-println person.id
-person.name = 'Another Name'
-println person.name
+session.discard()
+session.close()
 
-session.save()
-
-def anotherPerson = session.load(Person, 1)
-assert person == anotherPerson
+person.dump()
